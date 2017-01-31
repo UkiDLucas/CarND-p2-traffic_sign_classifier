047e31d4562bf3179351f3d459ca38e0710a2676
Ubuntu <ubuntu@ip-172-31-32-233.us-west-2.compute.internal>
2017-01-31 20:14:10 +0000

    adding keras

---
+++ Keras_german_traffic_signs_classifier.py
@@ -0,0 +1,37 @@
+
+# coding: utf-8
+
+# In[1]:
+
+from urllib.request import urlretrieve
+from os.path import isfile
+from tqdm import tqdm
+# conda install -c conda-forge tqdm
+
+
+# In[ ]:
+
+class DLProgress(tqdm):
+    last_block = 0
+
+    def hook(self, block_num=1, block_size=1, total_size=None):
+        self.total = total_size
+        self.update((block_num - self.last_block) * block_size)
+        self.last_block = block_num
+
+if not isfile('train.p'):
+    with DLProgress(unit='B', unit_scale=True, miniters=1, desc='Train Dataset') as pbar:
+        urlretrieve(
+            'https://s3.amazonaws.com/udacity-sdc/datasets/german_traffic_sign_benchmark/train.p',
+            'train.p',
+            pbar.hook)
+
+if not isfile('test.p'):
+    with DLProgress(unit='B', unit_scale=True, miniters=1, desc='Test Dataset') as pbar:
+        urlretrieve(
+            'https://s3.amazonaws.com/udacity-sdc/datasets/german_traffic_sign_benchmark/test.p',
+            'test.p',
+            pbar.hook)
+
+print('Training and Test data downloaded.')
+
---
+++ git_traffic_signs.sh
@@ -0,0 +1,13 @@
+
+# see what has changed on the server
+git status
+
+# if all files above (in RED) you want to synch
+# git add --all
+git add .gitignore
+
+git commit -m "adding git ignore file"
+
+git pull
+
+
