4ab81a073ee8e024fe3a95287dba0986d0609b1d
Uki D. Lucas <UkiDLucas@gmail.com>
2017-01-31 16:00:07 -0600

    adding stuff, no kerns import

--- Keras_german_traffic_signs_classifier.py
+++ Keras_german_traffic_signs_classifier.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 # conda install -c conda-forge tqdm
 
 
-# In[4]:
+# In[2]:
 
 # This is a wrapper class to show the progrees of the download.
 class DownloadProgress(tqdm):
@@ -28,7 +28,7 @@ class DownloadProgress(tqdm):
         self.last_block = block_num
 
 
-# In[8]:
+# In[3]:
 
 # urllib.request.urlretrieve(url[, filename[, reporthook[, data]]])
 
@@ -36,6 +36,7 @@ class DownloadProgress(tqdm):
 data_dir = "../DATA/german_traffic_signs/"
 
 file_name = 'train.p'
+
 if not isfile(data_dir + file_name):
     with DownloadProgress(unit='B', unit_scale=True, miniters=1, desc='Train Dataset') as pbar:
         urlretrieve(
@@ -45,6 +46,7 @@ if not isfile(data_dir + file_name):
 
         
 file_name = 'test.p'
+
 if not isfile(data_dir + file_name):
     with DownloadProgress(unit='B', unit_scale=True, miniters=1, desc='Test Dataset') as pbar:
         urlretrieve(
@@ -59,7 +61,7 @@ if not isfile(data_dir + file_name):
 print('Training and Test data downloaded at ' + data_dir)
 
 
-# In[9]:
+# In[4]:
 
 import pickle
 import numpy as np
@@ -72,7 +74,7 @@ tf.python.control_flow_ops = tf
 print('Modules loaded.')
 
 
-# In[11]:
+# In[5]:
 
 with open(data_dir + 'train.p', 'rb') as f:
     data = pickle.load(f)
@@ -88,7 +90,7 @@ assert np.array_equal(y_train, data['labels']), 'y_train not set to data[\'label
 print('Training data sets are loaded correctly')
 
 
-# In[13]:
+# In[6]:
 
 from sklearn.utils import shuffle
 X_train, y_train = shuffle(X_train, y_train) 
@@ -100,7 +102,7 @@ assert not np.array_equal(y_train, data['labels']), 'y_train not shuffled.'
 print('Tests passed.')
 
 
-# In[15]:
+# In[7]:
 
 def normalize_color_intensity(image_data):
     # desired range
@@ -112,10 +114,157 @@ def normalize_color_intensity(image_data):
     return a + ( ( (image_data - grayscale_min)*(b - a) )/( grayscale_max - grayscale_min ) )
 
 
-# In[16]:
+# In[8]:
 
 X_normalized = normalize_color_intensity(X_train)
 
+
+# In[9]:
+
+assert math.isclose(np.min(X_normalized), -0.5, abs_tol=1e-5) and math.isclose(np.max(X_normalized), 0.5, abs_tol=1e-5), 'The range of the training data is: {} to {}.  It must be -0.5 to 0.5'.format(np.min(X_normalized), np.max(X_normalized))
+print('Tests passed.')
+
+
+# In[10]:
+
+from sklearn.preprocessing import LabelBinarizer
+label_binarizer = LabelBinarizer()
+y_one_hot = label_binarizer.fit_transform(y_train)
+
+
+# In[11]:
+
+import collections
+
+assert y_one_hot.shape == (39209, 43), 'y_one_hot is not the correct shape.  It\'s {}, it should be (39209, 43)'.format(y_one_hot.shape)
+assert next((False for y in y_one_hot if collections.Counter(y) != {0: 42, 1: 1}), True), 'y_one_hot not one-hot encoded.'
+print('Tests passed.')
+
+
+# In[12]:
+
+from keras.models import Sequential
+
+
+
+# In[13]:
+
+# Create the Sequential model
+# The keras.models.Sequential class is a wrapper for the neural network model. 
+# Just like many of the class models in scikit-learn, it provides common functions like 
+# - fit()
+# - evaluate()
+# - compile() 
+
+model = Sequential()
+
+
+# In[15]:
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Flatten
+
+
+# In[ ]:
+
+# Create the Sequential model
+model = Sequential()
+
+# 1st Layer - Add a flatten layer
+model.add(Flatten(input_shape=(32, 32, 3)))
+
+# 2nd Layer - Add a fully connected layer
+model.add(Dense(100))
+
+# 3rd Layer - Add a ReLU activation layer
+model.add(Activation('relu'))
+
+# 4th Layer - Add a fully connected layer
+model.add(Dense(60))
+
+# 5th Layer - Add a ReLU activation layer
+model.add(Activation('relu'))
+
+
+# Keras will automatically infer the shape of all layers after the first layer. This means you only have to set the input dimensions for the first layer.
+# The first layer from above, model.add(Flatten(input_shape=(32, 32, 3))), sets the input dimension to (32, 32, 3) and output dimension to (3072=32*32*3). The second layer takes in the output of the first layer and sets the output dimenions to (100). This chain of passing output to the next layer continues until the last layer, which is the output of the model.
+
+# # Build a Multi-Layer Feedforward Network
+# Build a multi-layer feedforward neural network to classify the traffic sign images.
+# Set the first layer to a Flatten layer with the input_shape set to (32, 32, 3)
+# Set the second layer to Dense layer width to 128 output.
+# Use a ReLU activation function after the second layer.
+# Set the output layer width to 43, since there are 43 classes in the dataset.
+# Use a softmax activation function after the output layer.
+# To get started, review the Keras documentation about models and layers.
+# The Keras example of a Multi-Layer Perceptron network is similar to what you need to do here. Use that as a guide, but keep in mind that there are a number of differences.
+
+# In[16]:
+
+from keras.models import Sequential
+model = Sequential()
+# TODO: Build a Multi-layer feedforward neural network with Keras here.
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Flatten
+
+model.add(Flatten(input_shape=(32, 32, 3)))
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dense(43))
+model.add(Activation('softmax'))
+
+
+# In[17]:
+
+# STOP: Do not change the tests below. Your implementation should pass these tests.
+from keras.layers.core import Dense, Activation, Flatten
+from keras.activations import relu, softmax
+
+def check_layers(layers, true_layers):
+    assert len(true_layers) != 0, 'No layers found'
+    for layer_i in range(len(layers)):
+        assert isinstance(true_layers[layer_i], layers[layer_i]), 'Layer {} is not a {} layer'.format(layer_i+1, layers[layer_i].__name__)
+    assert len(true_layers) == len(layers), '{} layers found, should be {} layers'.format(len(true_layers), len(layers))
+
+check_layers([Flatten, Dense, Activation, Dense, Activation], model.layers)
+
+assert model.layers[0].input_shape == (None, 32, 32, 3), 'First layer input shape is wrong, it should be (32, 32, 3)'
+assert model.layers[1].output_shape == (None, 128), 'Second layer output is wrong, it should be (128)'
+assert model.layers[2].activation == relu, 'Third layer not a relu activation layer'
+assert model.layers[3].output_shape == (None, 43), 'Fourth layer output is wrong, it should be (43)'
+assert model.layers[4].activation == softmax, 'Fifth layer not a softmax activation layer'
+print('Tests passed.')
+
+
+# ## Training a Sequential Model
+# You built a multi-layer neural network in Keras, now let's look at training a neural network.
+# ```python
+# from keras.models import Sequential
+# from keras.layers.core import Dense, Activation
+# 
+# model = Sequential()
+# ...
+# 
+# # Configures the learning process and metrics
+# model.compile('sgd', 'mean_squared_error', ['accuracy'])
+# 
+# # Train the model
+# # History is a record of training loss and metrics
+# history = model.fit(x_train_data, Y_train_data, batch_size=128, nb_epoch=2, validation_split=0.2)
+# 
+# # Calculate test score
+# test_score = model.evaluate(x_test_data, Y_test_data)
+# ```
+# The code above configures, trains, and tests the model.  The line `model.compile('sgd', 'mean_squared_error', ['accuracy'])` configures the model's optimizer to `'sgd'`(stochastic gradient descent), the loss to `'mean_squared_error'`, and the metric to `'accuracy'`.  
+# 
+# You can find more optimizers [here](https://keras.io/optimizers/), loss functions [here](https://keras.io/objectives/#available-objectives), and more metrics [here](https://keras.io/metrics/#available-metrics).
+# 
+# To train the model, use the `fit()` function as shown in `model.fit(x_train_data, Y_train_data, batch_size=128, nb_epoch=2, validation_split=0.2)`.  The `validation_split` parameter will split a percentage of the training dataset to be used to validate the model.  The model can be further tested with the test dataset using the `evaluation()` function as shown in the last line.
+
+# ## Train the Network
+# 
+# 1. Compile the network using adam optimizer and categorical_crossentropy loss function.
+# 2. Train the network for ten epochs and validate with 20% of the training data.
 
 # In[ ]:
 
